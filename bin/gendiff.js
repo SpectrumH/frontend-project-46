519abd3d3dac670005e0847684cdd4fcf9957074
Gleb Kaplunskiy <gphenomen@gmail.com>
2023-11-07 10:40:02 +0300

    update gendiff.js added options and args

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,6 +7,14 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0');
+  .version('1.0.0')
+  .argument('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format');
+// program.command('gendiff')
+//   .description('Compares two configuration files and shows a difference.')
+// .action((str, options) => {
+//   const limit = options.first ? 1 : undefined;
+//   console.log(str.split(options.separator, limit));
+// });
 
 program.parse();
